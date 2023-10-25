76032064154d65808138a6aec9ffa29d1cafc97b
wcm4284 <83888850+wcm4284@users.noreply.github.com>
2023-10-25 16:53:22 -0500

    test

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
 
+int main()
+{
+    std::cout << "HEllo world" << std::endl;
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
