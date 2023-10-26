be922fb87557ed225b4d9096a470a3cea6901008
wcm4284 <83888850+wcm4284@users.noreply.github.com>
2023-10-26 01:38:25 -0500

    class for entities created

--- particle.cpp
+++ particle.cpp
@@ -1,1 +1,30 @@
+#include <SFML/Graphics.hpp>
 
+class Particle : public sf::Transformable {
+    struct {
+        float x_, y_;
+    } pos_, vel_, acc_;
+
+    float mass_, radius_;
+
+    Particle() {
+        vel_.x_ = 0;
+        vel_.y_ = 0;
+        acc_.x_ = 0;
+        acc_.y_ = 0;
+        radius_ = 1;
+        mass_ = 1;
+    }
+
+    Particle(float x, float y) {
+        pos_.x_ = x;
+        pos_.y_ = y;
+        Particle();
+    }
+
+    Particle(float x, float y, float mass, float radius) {
+        radius_ = radius;
+        mass_ = mass;
+        Particle(x, y);
+    }
+}
