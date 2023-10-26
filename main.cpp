06eaf966e93869fc90642a1f843be92adb301a51
wcm4284 <83888850+wcm4284@users.noreply.github.com>
2023-10-26 02:54:44 -0500

    working on rendering multiple shapes

--- main.cpp
+++ main.cpp
@@ -1,25 +1,41 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "particle.cpp"
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
+int c_NUMENTITIES = 100;
+
+
+int main() {
+    Particle entities[c_NUMENTITIES];
+
+    for (int i = 0; i < c_NUMENTITIES; ++i) {
+        entities[i] = * (new Particle(i + 2, i + 2, 1, 50));
+    }
+    
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Physics Engine");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+
+    
 
     while (window.isOpen())
     {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) window.close();
         }
 
         window.clear();
-        window.draw(shape);
+
+        for (int i = 0; i < c_NUMENTITIES; i++) {
+            sf::CircleShape shape(entities[i].radius);
+            shape.move(entities[i].pos.x, entities[i].pos.y);
+            shape.setFillColor(sf::Color::Green);
+            window.draw(shape);
+        }
         window.display();
     }
 
     return 0;
 }
+
