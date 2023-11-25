bdefb7b233dea44cba7df61c92bbe2c5a3462446
wcm4284 <83888850+wcm4284@users.noreply.github.com>
2023-11-24 22:08:29 -0600

    gravity sources implemented, bugs exist

--- gravity_source.cpp
+++ gravity_source.cpp
@@ -1,1 +1,27 @@
+#include <cmath>
+#include "gravity_source.h"
 
+
+void applyGravity(particle_t &dst, grav_source_t &src) {
+    float theta = atan2((src.pos->y - dst.pos->y), (src.pos->x - dst.pos->x));
+    float force = universalGrav * src.im / pow(getDistance(*src.pos, *dst.pos), 2);
+    dst.acc->x += force * cosf(theta);
+    dst.acc->y += force * sinf(theta);
+}
+
+bool detectCollision(grav_source_t &old, grav_source_t& newsrc) {
+    return getDistance(*old.pos, *newsrc.pos) <= old.ir + newsrc.ir;
+}
+
+grav_source_t create(const int x, const int y, float mass, float radius) {
+    grav_source_t newSource;
+    newSource.pos = (sf::Vector2f *) malloc(sizeof(sf::Vector2f));
+    *newSource.pos = {(float) x, (float) y};
+    newSource.ir = radius;
+    newSource.im = mass;
+    newSource.drawable.setFillColor(sf::Color::Black);
+    newSource.drawable.setPosition(*newSource.pos);
+    newSource.drawable.setRadius(newSource.ir);
+
+    return newSource;
+}
--- gravity_source.h
+++ gravity_source.h
@@ -2,12 +2,26 @@
 #define _GRAVITY_SOURCE_H_
 
 #include <SFML/Graphics.hpp>
+#include "particle.h"
+
+const int universalGrav = 200;
 
 typedef struct {
     sf::Vector2f *pos;
     sf::CircleShape drawable;
+    float ir;
     float im;
 } grav_source_t;
 
+void applyGravity(particle_t &dst, grav_source_t &src);
+
+/* 
+function to detect collisions between two gravity sources
+returns true if a collision is detected 
+*/
+// implemented in gravity_source.cpp
+bool detectCollision(grav_source_t &old, grav_source_t &newsrc);
+
+grav_source_t create(const int x, const int y, float mass, float radius);
     
 #endif
